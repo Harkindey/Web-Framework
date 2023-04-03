0fe150008182fa0c0fe9b4f57826c43b1de046bc
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-04-03 21:44:45 +0100

    triggering event Callbacks

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,17 @@ import { User } from "./models/User";
 const user = new User({name:'Beanne', age: 20})
 
 user.on('change', ()=>{
-
+console.log('change #1')
 })
 
 user.on('change', ()=>{
-
+    console.log('change #2')
 })
 
 user.on('click', ()=>{
-
+    console.log('Was clicked')
 })
 
-console.log(user);
+user.trigger('click')
+
+// console.log(user);
