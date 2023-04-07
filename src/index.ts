d04578beee2c9bf9638ce886c30d10fa1cba5404
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-04-07 16:36:58 +0100

    Composiition with nested Objects

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import { User } from "./models/User";
 const user = new User({name: 'Kuso', age: 20})
 
 user.save()
+user.events.on('change', () => {console.log('change')})
+user.events.trigger('change')
