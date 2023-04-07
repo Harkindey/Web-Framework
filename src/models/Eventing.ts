78aefdcee5fbb5a67261f6430d8b043ef2fc252e
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-04-07 16:09:24 +0100

    refactoring with composition

---
+++ src/models/Eventing.ts
@@ -0,0 +1,30 @@
+type Callback = ()=>void
+
+export class Eventing{
+    events: { [key:string]: Callback[]} = {};
+    on(eventName:string, callback: Callback): void{
+        {/* what i would have done
+        if(this.events[eventName] === undefined){
+                    this.events[eventName]=[callback]
+                } else {
+                    this.events[eventName].push(callback)
+                }
+        */} 
+                
+            const handlers = this.events[eventName] || []; //nice nice
+            handlers.push(callback);
+            this.events[eventName] = handlers;
+        }
+    
+        trigger(eventName: string):void {
+            const handlers = this.events[eventName];
+    
+            if(!handlers || handlers.length=== 0){
+                return
+            }
+    
+            handlers.forEach(callback=>{
+                callback();
+            })
+        }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -5,12 +5,8 @@ interface UserProps {
     age?:number
 }
 
-type Callback = ()=>void
-
 
 export class User {
-    events: { [key:string]: Callback[]} = {};
-
     constructor(private data: UserProps ){}
 
     get(propName: string): string|number {
@@ -19,32 +15,6 @@ export class User {
 
     set (update:UserProps): void {
         Object.assign(this.data, update);
-    }
-
-    on(eventName:string, callback: Callback): void{
-    {/* what i would have done
-    if(this.events[eventName] === undefined){
-                this.events[eventName]=[callback]
-            } else {
-                this.events[eventName].push(callback)
-            }
-    */} 
-            
-        const handlers = this.events[eventName] || []; //nice nice
-        handlers.push(callback);
-        this.events[eventName] = handlers;
-    }
-
-    trigger(eventName: string):void {
-        const handlers = this.events[eventName];
-
-        if(!handlers || handlers.length=== 0){
-            return
-        }
-
-        handlers.forEach(callback=>{
-            callback();
-        })
     }
 
     fetch():void {
