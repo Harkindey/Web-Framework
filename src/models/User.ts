1565e87e27f38615725b0d57af54662c4735f834
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-04-10 17:30:21 +0100

    connecting Sync Back to user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,18 @@
 
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 export interface UserProps { 
     id?: number
     name?: string;
     age?:number
 }
 
+const rootUrl = 'http://localhost:3000/users'
 
 export class User {
     public events: Eventing = new Eventing()
+    public sync: Sync<UserProps> = new Sync<UserProps>('http://localhost:3000/users')
+
     constructor(private data: UserProps ){}
 
     get(propName: string): string|number {
