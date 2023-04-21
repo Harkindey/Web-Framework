3ac56d97d816a7fcca64bfa9d9cf79ebbe834f41
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-04-21 13:25:51 +0100

    re-integrating attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,5 +13,13 @@ const rootUrl = 'http://localhost:3000/users'
 export class User {
     public events: Eventing = new Eventing()
     public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
-    public attributes:Attributes<UserProps> = new Attributes<UserProps>({id:5, name:'Bean', age:27})
+    public attributes:Attributes<UserProps> = new Attributes<UserProps>({
+        id:5, 
+        name:'Bean', 
+        age:27
+    })
+
+    constructor(attrs: UserProps){
+        this.attributes = new Attributes<UserProps>(attrs)
+    }
 }
