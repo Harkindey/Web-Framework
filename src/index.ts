5e5a5b09c4eab2089030f5841ad697967ae16486
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-04-21 16:24:16 +0100

    reminder on accessor

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,15 @@ import { User } from "./models/User";
 
 const user = new User({name: 'Kuso', age: 20})
 
-user.attributes.data
-user.sync.save()
+// quick reminder on accessors
+
+class Person {
+    constructor(public firstname: string, public lastname: string){}
+
+   get fullName(): string {
+        return `${this.firstname} ${this.lastname}`
+    }
+}
+
+const person = new Person('firstname', 'lastname');
+console.log(person.fullName);
