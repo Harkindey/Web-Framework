ed65d9448a8cb94edf990ca1fb6a7ce6314396e3
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-04-24 11:58:47 +0100

    how 'this' works

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { User } from "./models/User";
 
 const user = new User({name: 'Kuso', age: 20})
+// console.log(user.get('name'))
 
-user.on('change', ()=>{
+// Reminder of how 'this' works
 
-})
+const colors = {
+    color:'red',
+    printColor(){
+        console.log(this.color)
+    }
+}
+
+colors.printColor()
+
+const printColor = colors.printColor
+printColor()
