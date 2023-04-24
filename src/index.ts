5727cafe418354deffdfed939034a0fd8131d13e
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-04-24 12:21:45 +0100

    Setting Data while triggering

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ const user = new User({name: 'Kuso', age: 20})
 console.log(user.get('name'))
 
 user.on('change', ()=>{
-    console.log('User was changed');
+    console.log('User was changed, we probably need to update some HTML');
 })
 
-user.trigger('change');
+user.set({
+    name:'Hanjo'
+})
+console.log(user.get('name'))
