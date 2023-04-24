15c5b0a5120e9056905325e927a42ad9566947fd
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-04-24 12:10:37 +0100

    Solve Context issue

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
 import { User } from "./models/User";
 
 const user = new User({name: 'Kuso', age: 20})
-// console.log(user.get('name'))
+console.log(user.get('name'))
 
-// Reminder of how 'this' works
+user.on('change', ()=>{
+    console.log('User was changed');
+})
 
-const colors = {
-    color:'red',
-    printColor(){
-        console.log(this.color)
-    }
-}
-
-colors.printColor()
-
-const printColor = colors.printColor
-printColor()
+user.trigger('change');
