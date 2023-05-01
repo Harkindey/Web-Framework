d7b4efc76bbef89dac857c6710f2142de5e7a6a1
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-05-01 18:00:42 +0100

    Parsing User Json

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,7 +1,11 @@
+import axios, { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
-import { User } from "./User";
+import { User, UserProps } from "./User";
+
+
 
  export class Collections {
+    constructor(public rootUrl: string){}
     models: User[] = [];
     events: Eventing = new Eventing();
     get on(){
@@ -9,5 +13,15 @@ import { User } from "./User";
     }
     get trigger(){
         return this.events.trigger
+    }
+    fetch ():void{
+        axios.get(this.rootUrl).then((response: AxiosResponse)=>{
+            console.log(response)
+            this.models = response.data.map((element: UserProps) => {
+                console.log(element)
+                return User.buildUser(element)
+            });
+            this.trigger('change', this.models)
+        })    
     }
  }
