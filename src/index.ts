7ae557dbc65715bdfdbe90a39ccc1e34ba1ddc5b
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-05-02 19:41:26 +0100

    Generic user collection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Collections } from "./models/Collection"
+import { User, UserProps } from "./models/User"
 
-const collection = new Collections('http://localhost:3000/users')
+const collection = new Collections<User,UserProps>(
+    'http://localhost:3000/users',
+    (json: UserProps)=> User.buildUser(json)
+    )
 collection.on('change', (data)=>{
     console.log(collection,data)
 })
